5e0f7a23279bce9139de4461cdd91a047167e22d
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2021-10-09 16:31:46 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,33 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
+  attr_accessor :data
 
+  NEWLINE = "\n"
+  TAB = "\t"
+
+  def row_to_table(headers, row)
+    row.map.with_index { |cell, i| [headers[i], cell] }.to_h
   end
 
-  # to_tsv: converts @data into tsv string
-  # returns: String in TSV format
-  def to_tsv
+  def take_tsv(tsv)
+    rows = tsv.split(NEWLINE).map { |line| line.split(TAB) }
+    headers = rows.first
+    table_data = rows[1..]
+    @data = table_data.map { |row| row_to_table(headers, row) }
+    @data
+  end
 
+  def to_tsv
+    keys = @data[0].keys.join(TAB)
+    output = keys + NEWLINE
+    @data.each do |line|
+      output.concat(line.values.join(TAB))
+      output.concat(NEWLINE)
+    end
+    output
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  attr_accessor :data
+
+  NEWLINE = "\n"
+  TAB = "\t"
+
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+    @data
+  end
+
+  def to_yaml
+    @data = @data.to_yaml
+    @data
+  end
+end
